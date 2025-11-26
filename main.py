e1bfa7660e795e2c3d3ce5a8d0f0de8386a5154c
atmosfer <jfrysayna2@gmail.com>
2025-11-25 18:16:09 -0800

    set the main page

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import flet as ft
 from information_page import InformationPage
 from device_page import DevicePage
 from device_section_page import DeviceSectionPage
 from section_detail_page import SectionDetailPage
-import flet as ft
+
 
 def main(page: ft.Page):
     page.title = "GT11 ELEC Form"
@@ -14,29 +15,36 @@ def main(page: ft.Page):
     if page.session.get("form_data") is None:
         page.session.set("form_data", {})
 
+    # ---------------------- ROUTING -------------------------
     def route_change(e: ft.RouteChangeEvent):
-        page.views.clear()  # مهم
+        page.views.clear()
 
         if page.route == "/" or page.route == "/information":
             page.views.append(InformationPage(page))
+
         elif page.route == "/device":
             page.views.append(DevicePage(page))
+
         elif page.route == "/sections":
             page.views.append(DeviceSectionPage(page))
+
         elif page.route == "/section_detail":
             page.views.append(SectionDetailPage(page))
 
-        page.update()  # این خط هم حیاتیه!
+        page.update()
 
-        def view_pop(e):
-            if len(page.views) > 1:
-                page.views.pop()
-                page.go(page.views[-1].route)
+    # ---------------------- BACK BUTTON ---------------------
+    def view_pop(e):
+        if len(page.views) > 1:
+            page.views.pop()
+            page.go(page.views[-1].route)
 
-        page.on_route_change = route_change
-        page.on_view_pop = view_pop
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
 
-        page.go("/")
+    page.go("/")   # <--- این خط باید آخر باشد!
+
 
 ft.app(target=main)
+
 
